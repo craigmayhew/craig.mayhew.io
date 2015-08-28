c5680ca8aa970fe4cf668082311b981469b8a9fa
craigmayhew <craig@mayhew.io>
2015-08-29 00:31:06 +0100

    Added nl2br to individual blog past pages

--- tools/createstatic.php
+++ tools/createstatic.php
@@ -154,7 +154,7 @@ class builder{
           '<h2>'.$json['title'].'</h2>'.
           'by Craig Mayhew on '.date('D dS M Y',strtotime($json['date'])).' under '.implode(', ',$json['categories']).
           '<br /><br /><br />'.$json['content'].'<br /><br /><br />';
-        $page->setContent($content.$tags.nl2br($comments));
+        $page->setContent(nl2br($content).$tags.nl2br($comments));
         $page->setSideNav($this->sideNav);
         $content = $page->build();
         $this->generateFile($this->destinationFolder.'blog/'.$json['name'].'/index.html',$content);

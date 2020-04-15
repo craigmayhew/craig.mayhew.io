55adda09013bd2a3e1bc0577e9a9009bef71b98d
Craig Mayhew <craig@mayhew.io>
2020-04-15 19:00:20 +0100

    Drift chat removed

--- tools/build.php
+++ tools/build.php
@@ -422,28 +422,6 @@ class page{
             '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">'.
             '<link rel="stylesheet" href="'.$relativePath.'css/font-awesome.min.css">'.
             '<link rel="stylesheet" href="'.$relativePath.'css/style.css">'.
-              '<script>
-                !function() {
-                  var t;
-                  if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0, 
-                  t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-                  t.factory = function(e) {
-                    return function() {
-                      var n;
-                      return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
-                    };
-                  }, t.methods.forEach(function(e) {
-                    t[e] = t.factory(e);
-                  }), t.load = function(t) {
-                    var e, n, o, i;
-                    e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"), 
-                    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js", 
-                    n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
-                  });
-                }();
-                drift.SNIPPET_VERSION = \'0.3.1\';
-                drift.load(\'s83msn8usbp9\');
-                </script>'.
         '</head>'.
         '<body>'.
       '<div id="fb-root"></div>'.
